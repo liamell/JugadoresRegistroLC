62a4d9559f69a75d90be899502349dd89710ea7e
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-09-18 14:24:01 -0400

    Agregue partidas al mainactivity

--- app/src/main/java/edu/ucne/jugadoresregistrolc/MainActivity.kt
+++ app/src/main/java/edu/ucne/jugadoresregistrolc/MainActivity.kt
@@ -1,30 +1,21 @@
+// MainActivity.kt
 package edu.ucne.jugadoresregistrolc
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.rememberNavController
 import androidx.room.Room
 import edu.ucne.jugadoresregistrolc.data.database.JugadorDb
-import edu.ucne.jugadoresregistrolc.data.entities.JugadorEntity
-import edu.ucne.jugadoresregistrolc.data.presentation.JugadorListScreen
 import edu.ucne.jugadoresregistrolc.data.repositories.JugadorRepository
-import edu.ucne.jugadoresregistrolc.data.presentation.JugadorScreen
+import edu.ucne.jugadoresregistrolc.data.repositories.PartidaRepository
+import edu.ucne.jugadoresregistrolc.navigation.AppNavHost
 import edu.ucne.jugadoresregistrolc.ui.theme.JugadoresRegistroLCTheme
-
-
-
 
 class MainActivity : ComponentActivity() {
     private lateinit var jugadorRepository: JugadorRepository
+    private lateinit var partidaRepository: PartidaRepository
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,31 +27,20 @@ class MainActivity : ComponentActivity() {
             "Jugadores.db"
         ).fallbackToDestructiveMigration()
             .build()
+
         jugadorRepository = JugadorRepository(jugadorDb.jugadorDao())
+        partidaRepository = PartidaRepository(jugadorDb.partidaDao())
 
         setContent {
-            JugadoresRegistroLCTheme { // Aquí se cierra correctamente solo con una llave al final del tema
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Column(
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .padding(innerPadding)
-                    ) {
-                        JugadorScreen(jugadorRepository)
-                    }
-                }
-            }
-        }
-    }
+            JugadoresRegistroLCTheme {
+                val navController = rememberNavController()
 
-    @Preview(showBackground = true)
-    @Composable
-    fun GreetingPreview() {
-        JugadoresRegistroLCTheme {
-            val jugadorList = listOf(
-                JugadorEntity(1, "L i a mell", 15112003),
-            )
-            JugadorListScreen(jugadorList)
+                AppNavHost(
+                    navController = navController,
+                    jugadorRepository = jugadorRepository,
+                    partidaRepository = partidaRepository
+                )
+            }
         }
     }
 }
