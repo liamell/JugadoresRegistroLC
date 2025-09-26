607447bc187380cb311b76eb7f7c5179da10799d
Liamell Cruz Disla <liamell_cruz@ucne.edu.do>
2025-09-26 15:42:15 -0400

    Partida view model modificada

--- app/src/main/java/edu/ucne/jugadoresregistrolc/presentation/PartidaScreens/PartidaViewModel.kt
+++ app/src/main/java/edu/ucne/jugadoresregistrolc/presentation/PartidaScreens/PartidaViewModel.kt
@@ -1,56 +1,31 @@
-package edu.ucne.jugadoresregistrolc.data.presentation.PartidaScreens
+package edu.ucne.jugadoresregistrolc.presentation.PartidaScreens
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import dagger.hilt.android.lifecycle.HiltViewModel
-import edu.ucne.jugadoresregistrolc.data.entities.JugadorEntity
 import edu.ucne.jugadoresregistrolc.data.entities.PartidaEntity
 import edu.ucne.jugadoresregistrolc.data.repositories.JugadorRepository
 import edu.ucne.jugadoresregistrolc.data.repositories.PartidaRepository
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.stateIn
-import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.Date
 import java.util.Locale
 import javax.inject.Inject
-
 @HiltViewModel
 class PartidaViewModel @Inject constructor(
     private val repository: PartidaRepository,
     private val jugadorRepository: JugadorRepository
 
 ) : ViewModel() {
-    private val _uiState = MutableStateFlow(JugadorUiState())
-    val uiState = _uiState.asStateFlow()
+
 
     init {
-        getJugadores()
+        //getJugadores()
     }
 
-     fun guardarJugador(){
-        viewModelScope.launch {
-            jugadorRepository.save(_uiState.value.toJugadorEntity())
-        }
-    }
-    private fun getJugadores(){
-        viewModelScope.launch {
-            try {
-                jugadorRepository.getAll().collect{jugadores ->
-                    _uiState.update {
-                        it.copy(listajugadores = jugadores)
-                    }
-                }
-            } catch (e: Exception){
-                Log.e("ViewModel", "Error obteniendo cuentas: ${e.message}", e)
-                e.printStackTrace()
-            }
-        }
-    }
+
     val partidas = repository.getAll()
 
         .stateIn(viewModelScope, SharingStarted.Companion.WhileSubscribed(5000), emptyList())
@@ -68,13 +43,6 @@ class PartidaViewModel @Inject constructor(
             repository.insert(partida)
         }
     }
-
-    data class JugadorUiState(
-        val jugadorId: Int? = null,
-        val nombre: String = "",
-        val partidas: Int = 0,
-        val listajugadores: List<JugadorEntity> = emptyList()
-    )
 
     fun updatePartida(partida: PartidaEntity) {
         viewModelScope.launch {
@@ -100,33 +68,4 @@ class PartidaViewModel @Inject constructor(
             }
         }
     }
-    fun JugadorUiState.toJugadorEntity() = JugadorEntity(
-        jugadorId = jugadorId,
-        nombre = nombre,
-        partidas = partidas
-    )
-
-
-
-    fun onEvent(event: JugadorUiEvent ){
-        when (event){
-            is JugadorUiEvent.NombreChanged -> {
-                _uiState.update {
-                    it.copy(nombre = event.nombre)
-                }
-            }
-            is JugadorUiEvent.PartidaChange -> {
-                _uiState.update {
-                    it.copy(partidas = event.partida)
-                }
-            }
-            JugadorUiEvent.Save -> {
-                viewModelScope.launch {
-                    jugadorRepository.save(_uiState.value.toJugadorEntity())
-                }
-            }
-        }
-    }
 }
-
-
